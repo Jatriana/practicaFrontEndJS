6c036d7ed0f151c7acee3ed001ef5f5c8c1d0847
jaime Triana <zubov2000@gmail.com>
2021-02-14 20:41:17 +0100

    pruebo con fetch desde el inex leer los dato que  tendo en la url que he creado, uso los dos formas con promesa de promesa response y async await

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import { anuncioView } from "./view/view.js";
 import dataService from "./service/DataService.js";
 
-window.addEventListener("DOMContentLoaded", (evento) => {
+window.addEventListener("DOMContentLoaded", async (evento) => {
   const loader = document.querySelector(".lds-ring");
   loader.classList.add("hidden");
 
@@ -19,4 +19,22 @@ window.addEventListener("DOMContentLoaded", (evento) => {
     console.error("no se cargan los anuncios");
   };
   dataService.getAnuncios().then(cargarAnuncios).catch(avisarDelError);
+
+
+    const url =  'https://gist.githubusercontent.com/Jatriana/915538990ce0448e1b44a25d0b43ceb2/raw/0b17ed63638cf90dbd4ebe0be073b90372de7654/anuncios.json';
+    const response =  await fetch(url);
+    const data  = await response.json();
+    console.log('esto son los datos', data);
+
+    // fetch(url).then(respuesta =>{
+    //     console.log('respuesta recibida', respuesta);
+    //     respuesta.json().then((data) =>{
+    //         console.log('estos son los datos,', data)
+    //     }).catch((error)=>{
+
+    //     })
+    // }).catch(error => {
+    //     console.error('la peticion ha fallado', error);
+    // })
+
 });
