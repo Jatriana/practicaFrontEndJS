c0c41d2b14fa24f006e74ae15dd9f37967ea4cee
jaime Triana <zubov2000@gmail.com>
2021-02-14 21:18:29 +0100

    uso el async await en el dataservice para llamar los datos de la url y devolver la promesa  de getanuncios , entonces cargar Anuncios en la vista

--- js/index.js
+++ js/index.js
@@ -20,21 +20,5 @@ window.addEventListener("DOMContentLoaded", async (evento) => {
   };
   dataService.getAnuncios().then(cargarAnuncios).catch(avisarDelError);
 
-
-    const url =  'https://gist.githubusercontent.com/Jatriana/915538990ce0448e1b44a25d0b43ceb2/raw/0b17ed63638cf90dbd4ebe0be073b90372de7654/anuncios.json';
-    const response =  await fetch(url);
-    const data  = await response.json();
-    console.log('esto son los datos', data);
-
-    // fetch(url).then(respuesta =>{
-    //     console.log('respuesta recibida', respuesta);
-    //     respuesta.json().then((data) =>{
-    //         console.log('estos son los datos,', data)
-    //     }).catch((error)=>{
-
-    //     })
-    // }).catch(error => {
-    //     console.error('la peticion ha fallado', error);
-    // })
-
+   
 });
--- js/service/DataService.js
+++ js/service/DataService.js
@@ -1,84 +1,18 @@
 
-function getFockinAnuncios(){
 
-    return  [
-        {
-          nombre: "bicicleta",
-          operacion: "venta",
-          precio: 230.15,
-          descripcion:
-            "Vendo bicicleta nueva, solo se le ha dado un uso. La vendo por no usarla ",
-          foto: "bici.jpg",
-          tags: ["lifestyle", "movilidad", "articulos"],
-        },
-        {
-          nombre: "moto",
-          operacion: "compra",
-          precio: 1650.0,
-          descripcion: " Compro motro Marca Rata, usada de no me importa el estado",
-          foto: "moto.jpg",
-          tags: ["lifestyle", "movilidad", "articulos"],
-        },
-        {
-          nombre: "coche",
-          operacion: "venta",
-          precio: 11650.0,
-          descripcion: "Peugeot 406 Sedan en Toledo",
-          foto: "cpche.jpg",
-          tags: ["lifestyle", "movilidad", "articulos"],
-        },
-        {
-          nombre: "cama",
-          operacion: "compra",
-          precio: 650.0,
-          descripcion:
-            "able abatible. Gran capacidad. Colchón Flex. Medidas 150x190",
-          foto: "cama.jpg",
-          tags: ["lifestyle", "house", "articulos"],
-        },
-        {
-          nombre: "Iphone 6S",
-          operacion: "venta",
-          precio: 500,
-          descripcion: "Iphone 6s de 32gb Prácticamente sin uso.",
-          foto: "iphone6s.jpg",
-          tags: ["lifestyle", "mobile", "articulos"],
-        }
-      ];
-}
-
+const url = 'https://gist.githubusercontent.com/Jatriana/915538990ce0448e1b44a25d0b43ceb2/raw/0b17ed63638cf90dbd4ebe0be073b90372de7654/anuncios.json';
 
 
 
 
 export default {
-  getAnuncios: () => {
-    //   fetch('../lib/db.json')
-    //   .then(respuetas =>respuetas.json())
-    //   .then(respuesta => console.log(respuesta));
-    // const response = await fetch('../lib/db.json');
-    //         const data = response.json();
-    //         console.log(data)
-    //         return data;
-    //     }
-   
-    const promise = new Promise((resolve, reject) => {
-        // resolve -> callback cuando todo va bien
-        // reject -> callback cuando algo va mal
-        setTimeout(() => {
-            const anuncios = getFockinAnuncios();
-            if (anuncios.length === 0) {
-                reject(anuncios);
-            } else {
-                resolve(anuncios);
-            }
-        }, Math.random() * 5000)
-    });
-    return promise
+    
+    getAnuncios: async () => {
+      
+        const respuesta = await fetch(url);
+        const datos = await respuesta.json();
+        return datos
 
-
-
-
-
+        
     }
 }
